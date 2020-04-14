3ff4b84756784d522c4687f163cbfaad882a80d6
FilipKudlacz <kudlacz1998@gmail.com>
2020-04-14 15:06:30 +0200

    Add main view

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main
+  },
+  methods: {
+    console () {
+      console.log('hello');
+    }
   }
-}
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  background-color: red;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="app-wrapper">
+    <div class="intro-wrapper">
+      <img
+          alt="Pokemon logo"
+          src="./../assets/1200pxlogo.svg.png"
+      >
+      <div class="pokeball" />
+    </div>
+    <div class="main-container">
+      <input
+          v-model="searchPhrase"
+          type="text"
+          placeholder="pokemon name"
+      >
+      <Results
+          :resultsList="pokemonArray"
+          :currentPage="currentPage"
+          :resultsPerPage="resultsPerPage"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Results from './Results';
+import axios from 'axios';
+
+const url = 'https://pokeapi.co/api/v2/';
+
+export default {
+  name: 'Main',
+  components: {
+    Results
+  },
+  data () {
+    return {
+      searchPhrase: '',
+      pokemonArray: [],
+      currentPage: 0,
+      resultsPerPage: 50
+    };
+  },
+  mounted () {
+    const self = this;
+
+    axios.get(url + 'pokemon/?offset=0&limit=964')
+      .then(res => {
+        this.pokemonArray = res.data.results;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.app-wrapper {
+  margin-top: 15px;
+  .intro-wrapper {
+    background-color: red;
+    height: 30vh;
+
+    img {
+      height: 25vh;
+    }
+
+    &::after {
+      content: '';
+      position: absolute;
+      top: 30vh;
+      left: 0;
+      height: 35px;
+      width: 100vw;
+      background-color: black;
+    }
+
+    .pokeball {
+      position: relative;
+      width: 120px;
+      height: 120px;
+      background-color: black;
+      border-radius: 50%;
+      left: calc(50vw - 60px);
+      top: -20px;
+
+      &::after {
+        position: absolute;
+        top: 30px;
+        left: 30px;
+        content: '';
+        width: 60px;
+        height: 60px;
+        background-color: white;
+        border-radius: 50%;
+        z-index: 2;
+      }
+    }
+  }
+  .main-container {
+    width: 100vw;
+    background-color: white;
+
+    input {
+      margin-top: 200px;
+      height: 40px;
+      width: 300px;
+      font-size: 20px;
+      border: solid black;
+      border-width: 0 0 2px 0;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+}
+</style>
